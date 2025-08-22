c31ec1524931e7c3e5c44f1984a68167267ed08b
alice <alice.ternois@orange.fr>
2025-08-22 17:47:20 +0200

    majvg0.3

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -35,6 +35,10 @@ class Produit
     #[ORM\Column]
     private ?bool $actif = null;
 
+    #[ORM\ManyToOne(inversedBy: 'produits')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?SousCategorie $souscategorie = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -120,6 +124,18 @@ class Produit
     public function setActif(bool $actif): static
     {
         $this->actif = $actif;
+
+        return $this;
+    }
+
+    public function getSoussouscategorie(): ?SousCategorie
+    {
+        return $this->souscategorie;
+    }
+
+    public function setSoussouscategorie(?SousCategorie $souscategorie): static
+    {
+        $this->souscategorie = $souscategorie;
 
         return $this;
     }
